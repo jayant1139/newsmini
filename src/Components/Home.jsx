911ffd3ac82c912f793d6216327bb88513fb70f8
Jayant <jayantsingh331@gmail.com>
2022-01-28 23:44:26 +0530

    nb

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,19 +1,86 @@
-import React from 'react';
+import React from "react";
 
 function Home() {
-  return(
-   <>
-   <div className="main-header">
-       <div className="heading"><img src={process.env.PUBLIC_URL+"/newsmini.svg"} alt="" srcset="" /></div>
-       <div className="heading heading-right">
-           <div className="heading-items"><p>Finance</p></div>
-           <div className="heading-items"><p>Entertainment</p></div>
-           <div className="heading-items"><p>Sports</p></div>
-           <div className="heading-items"><p>Politics</p></div>
-       </div>
-   </div>
-       
-       </>
+  return (
+    <>
+      <div className="main-header">
+        <div className="heading">
+          <img
+            src={process.env.PUBLIC_URL + "/newsmini.svg"}
+            alt=""
+            srcset=""
+          />
+        </div>
+        <div className="heading heading-right">
+          <a className="heading-items">
+            <p>Finance</p>
+          </a>
+          <a className="heading-items">
+            <p>Entertainment</p>
+          </a>
+          <a className="heading-items">
+            <p>Sports</p>
+          </a>
+          <a className="heading-items">
+            <p>Politics</p>
+          </a>
+        </div>
+      </div>
+
+      <div className="desc-container">
+        <div className="desc-items">
+          <p>
+            Finance is a broad term that describes activities associated with
+            banking, leverage or debt, credit, capital markets, money, and
+            investments. Basically, finance represents money management and the
+            process of acquiring needed funds.
+          </p>
+        </div>
+        <div className="desc-items desc-bg-shadow">
+          <img src={process.env.PUBLIC_URL + "/Images/finance.png"} alt="" />
+        </div>
+      </div>
+
+      <div className="desc-container">
+        <div className="desc-items desc-bg-shadow">
+          <img src={process.env.PUBLIC_URL + "/Images/finance.png"} alt="" />
+        </div>
+        <div className="desc-items">
+          <p>
+          Sport is an activity involving physical exertion and
+           skill in which an individual or team competes against another or others for entertainment.
+          </p>
+        </div>
+      </div>
+
+      <div className="desc-container">
+        <div className="desc-items">
+          <p>
+          Entertainment is a form of activity that holds the attention and interest of an audience, 
+          or gives pleasure and delight. It can be an idea or a task, but is more likely to be
+           one of the activities or events that have developed over thousands of years specifically for the purpose
+            of keeping an audience's attention.
+          </p>
+        </div>
+        <div className="desc-items desc-bg-shadow">
+          <img src={process.env.PUBLIC_URL + "/Images/finance.png"} alt="" />
+        </div>
+      </div>
+
+      <div className="desc-container">
+        <div className="desc-items desc-bg-shadow">
+          <img src={process.env.PUBLIC_URL + "/Images/finance.png"} alt="" />
+        </div>
+        <div className="desc-items">
+          <p>
+          Politics is the way that people living in groups make decisions. 
+          Politics is about making agreements between people so that they can live together in groups 
+          such as tribes, cities, or countries. 
+          Politicians, and sometimes other people, may get together to form a government.
+          </p>
+        </div>
+      </div>
+    </>
   );
 }
 
